72db4e12b3667aa12b62dc982a51a01cbcf2db02
ChaseTerzian <chaseterzian@gmail.com>
2015-04-10 09:21:18 -0600

    added expire_hsts to try to fix https issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :expire_hsts
+
+  private
+
+    def expire_hsts
+      response.headers["Strict-Transport-Security"] = 'max-age=0'
+    end
+
 end
