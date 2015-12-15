cd277ec5f6b0513c662b9f7be3053eac62ffd542
ChaseTerzian <chaseterzian@gmail.com>
2015-12-14 23:57:14 -0700

    CSS changes, style changes, added JS, cleaned up

---
+++ app/assets/javascripts/queryStyle.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+	
+		$('#hide-this').hide();
+		$('#top-bar').hide();
+		$('#footer-bar').hide();
+		$('#middle-bar').hide();
+		$('#whiteboard-letters').hide();
+		$('footer').hide();
+
+		$('#whiteboard-letters').fadeIn(1200);
+
+		setTimeout(function() {
+		$('#footer-index').fadeIn(4000);
+		$('#hide-this').fadeIn(4000);
+		},1500);
+
+		setTimeout(function() {
+		$('#footer-bar').fadeIn(4000);
+		},1500);
+
+		setTimeout(function() {
+		$('#middle-bar').fadeIn(4000);
+		},1700);
+
+		setTimeout(function() {
+		$('#top-bar').fadeIn(4000);
+		},2000);
+
+		$("#room-fade").hide();
+		$("#room-fade").fadeIn(1000);
+
+
+});
